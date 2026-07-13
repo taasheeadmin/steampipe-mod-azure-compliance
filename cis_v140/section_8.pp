a5b4384df1806fcca6b5f058498ca41212e5e94a
taasheeadmin <appadmin@taashee.com>
2026-07-13 13:40:45 +0530

    Added severity levels

--- cis_v140/section_8.pp
+++ cis_v140/section_8.pp
@@ -27,7 +27,8 @@ control "cis_v140_8_1" {
   description   = "Ensure that all Keys in Role Based Access Control (RBAC) Azure Key Vaults have an expiration time set."
   query         = query.keyvault_with_rbac_key_expiration_set
   documentation = file("./cis_v140/docs/cis_v140_8_1.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v140_8_common_tags, {
     cis_item_id = "8.1"
     cis_level   = "1"
@@ -41,7 +42,8 @@ control "cis_v140_8_2" {
   description   = "Ensure that all Keys in Non Role Based Access Control (RBAC) Azure Key Vaults have an expiration time set."
   query         = query.keyvault_with_non_rbac_key_expiration_set
   documentation = file("./cis_v140/docs/cis_v140_8_2.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v140_8_common_tags, {
     cis_item_id = "8.2"
     cis_level   = "1"
@@ -55,7 +57,8 @@ control "cis_v140_8_3" {
   description   = "Ensure that all Secrets in Role Based Access Control (RBAC) Azure Key Vaults have an expiration time set"
   query         = query.keyvault_with_rbac_secret_expiration_set
   documentation = file("./cis_v140/docs/cis_v140_8_3.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v140_8_common_tags, {
     cis_item_id = "8.3"
     cis_level   = "1"
@@ -69,7 +72,8 @@ control "cis_v140_8_4" {
   description   = "Ensure that all Secrets in Non Role Based Access Control (RBAC) Azure Key Vaults have an expiration time set."
   query         = query.keyvault_with_non_rbac_secret_expiration_set
   documentation = file("./cis_v140/docs/cis_v140_8_4.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v140_8_common_tags, {
     cis_item_id = "8.4"
     cis_level   = "1"
@@ -83,7 +87,8 @@ control "cis_v140_8_5" {
   description   = "Resource Manager Locks provide a way for administrators to lock down Azure resources to prevent deletion of, or modifications to, a resource. These locks sit outside of the Role Based Access Controls (RBAC) hierarchy and, when applied, will place restrictions on the resource for all users. These locks are very useful when there is an important resource in a subscription that users should not be able to delete or change. Locks can help prevent accidental and malicious changes or deletion."
   query         = query.manual_control
   documentation = file("./cis_v140/docs/cis_v140_8_5.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v140_8_common_tags, {
     cis_item_id = "8.5"
     cis_level   = "2"
@@ -97,7 +102,8 @@ control "cis_v140_8_6" {
   description   = "The key vault contains object keys, secrets and certificates. Accidental unavailability of a key vault can cause immediate data loss or loss of security functions (authentication, validation, verification, non-repudiation, etc.) supported by the key vault objects. It is recommended the key vault be made recoverable by enabling the \"Do Not Purge\" and \"Soft Delete\" functions."
   query         = query.keyvault_vault_recoverable
   documentation = file("./cis_v140/docs/cis_v140_8_6.md")
-
+  severity = "critical"
+  
   tags = merge(local.cis_v140_8_common_tags, {
     cis_item_id = "8.6"
     cis_level   = "1"
@@ -111,7 +117,8 @@ control "cis_v140_8_7" {
   description   = "Ensure that RBAC is enabled on all Azure Kubernetes Services Instances."
   query         = query.keyvault_vault_recoverable
   documentation = file("./cis_v140/docs/cis_v140_8_5.md")
-
+  severity = "critical"
+  
   tags = merge(local.cis_v140_8_common_tags, {
     cis_item_id = "8.7"
     cis_level   = "1"
