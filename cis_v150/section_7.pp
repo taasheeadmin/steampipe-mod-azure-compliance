379c96bc808048efc40778d80d589a247536fb67
taasheeadmin <appadmin@taashee.com>
2026-07-15 14:27:17 +0530

    Added severity levels

--- cis_v150/section_7.pp
+++ cis_v150/section_7.pp
@@ -27,7 +27,8 @@ control "cis_v150_7_1" {
   description   = "Migrate BLOB based VHD's to Managed Disks on Virtual Machines to exploit the default features of this configuration."
   query         = query.compute_vm_utilizing_managed_disk
   documentation = file("./cis_v150/docs/cis_v150_7_1.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_7_common_tags, {
     cis_item_id = "7.1"
     cis_level   = "1"
@@ -41,7 +42,8 @@ control "cis_v150_7_2" {
   description   = "Ensure that OS disks (boot volumes) and data disks (non-boot volumes) are encrypted with CMK (Customer Managed Keys). Customer Managed keys can be either ADE or Server Side Encryption(SSE)."
   query         = query.compute_os_and_data_disk_encrypted_with_cmk
   documentation = file("./cis_v150/docs/cis_v150_7_2.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_7_common_tags, {
     cis_item_id = "7.2"
     cis_level   = "2"
@@ -55,7 +57,8 @@ control "cis_v150_7_3" {
   description   = "Ensure that unattached disks in a subscription are encrypted with a Customer Managed Key (CMK)."
   query         = query.compute_unattached_disk_encrypted_with_cmk
   documentation = file("./cis_v150/docs/cis_v150_7_3.md")
-
+  severity = "medium"
+  
   tags = merge(local.cis_v150_7_common_tags, {
     cis_item_id = "7.3"
     cis_level   = "2"
@@ -69,7 +72,8 @@ control "cis_v150_7_4" {
   description   = "For added security only install organization-approved extensions on VMs."
   query         = query.manual_control
   documentation = file("./cis_v150/docs/cis_v150_7_4.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_7_common_tags, {
     cis_item_id = "7.4"
     cis_level   = "1"
@@ -83,7 +87,8 @@ control "cis_v150_7_5" {
   description   = "Install endpoint protection for all virtual machines."
   query         = query.manual_control
   documentation = file("./cis_v150/docs/cis_v150_7_5.md")
-
+  severity = "critical"
+  
   tags = merge(local.cis_v150_7_common_tags, {
     cis_item_id = "7.5"
     cis_level   = "2"
@@ -97,7 +102,8 @@ control "cis_v150_7_6" {
   description   = "VHD (Virtual Hard Disks) are stored in blob storage and are the old-style disks that were attached to Virtual Machines. The blob VHD was then leased to the VM. By default, storage accounts are not encrypted, and Microsoft Defender will then recommend that the OS disks should be encrypted. Storage accounts can be encrypted as a whole using PMK or CMK. This should be turned on for storage accounts containing VHDs."
   query         = query.manual_control
   documentation = file("./cis_v150/docs/cis_v150_7_6.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_7_common_tags, {
     cis_item_id = "7.6"
     cis_level   = "2"
