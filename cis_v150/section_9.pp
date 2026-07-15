b3e50a8e3e7ce57f8e6b071aab792249ce31ced8
taasheeadmin <appadmin@taashee.com>
2026-07-15 14:49:12 +0530

    Added severity levels

--- cis_v150/section_9.pp
+++ cis_v150/section_9.pp
@@ -32,7 +32,8 @@ control "cis_v150_9_1" {
   description   = "Azure App Service Authentication is a feature that can prevent anonymous HTTP requests from reaching the API app, or authenticate those that have tokens before they reach the API app. If an anonymous request is received from a browser, App Service will redirect to a logon page. To handle the logon process, a choice from a set of identity providers can be made, or a custom authentication mechanism can be implemented."
   query         = query.appservice_authentication_enabled
   documentation = file("./cis_v150/docs/cis_v150_9_1.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.1"
     cis_level   = "2"
@@ -46,7 +47,8 @@ control "cis_v150_9_2" {
   description   = "Azure Web Apps allows sites to run under both HTTP and HTTPS by default. Web apps can be accessed by anyone using non-secure HTTP links by default. Non-secure HTTP requests can be restricted and all HTTP requests redirected to the secure HTTPS port. It is recommended to enforce HTTPS-only traffic."
   query         = query.appservice_web_app_use_https
   documentation = file("./cis_v150/docs/cis_v150_9_2.md")
-
+  severity = "critical"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.2"
     cis_level   = "1"
@@ -60,7 +62,8 @@ control "cis_v150_9_3" {
   description   = "The TLS(Transport Layer Security) protocol secures transmission of data over the internet using standard encryption technology. Encryption should be set with the latest version of TLS. App service allows TLS 1.2 by default, which is the recommended TLS level by industry standards, such as PCI DSS."
   query         = query.appservice_web_app_latest_tls_version
   documentation = file("./cis_v150/docs/cis_v150_9_3.md")
-
+  severity = "critical"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.3"
     cis_level   = "1"
@@ -74,7 +77,8 @@ control "cis_v150_9_4" {
   description   = "Client certificates allow for the app to request a certificate for incoming requests. Only clients that have a valid certificate will be able to reach the app."
   query         = query.appservice_web_app_incoming_client_cert_on
   documentation = file("./cis_v150/docs/cis_v150_9_4.md")
-
+  severity = "medium"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.4"
     cis_level   = "2"
@@ -88,7 +92,8 @@ control "cis_v150_9_5" {
   description   = "Managed service identity in App Service makes the app more secure by eliminating secrets from the app, such as credentials in the connection strings. When registering with Azure Active Directory in the app service, the app will connect to other Azure services securely without the need of username and passwords."
   query         = query.appservice_web_app_register_with_active_directory_enabled
   documentation = file("./cis_v150/docs/cis_v150_9_5.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.5"
     cis_level   = "1"
@@ -102,7 +107,8 @@ control "cis_v150_9_6" {
   description   = "Periodically newer versions are released for PHP software either due to security flaws or to include additional functionality. Using the latest PHP version for web apps is recommended in order to take advantage of security fixes, if any, and/or additional functionalities of the newer version."
   query         = query.manual_control
   documentation = file("./cis_v150/docs/cis_v150_9_6.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.6"
     cis_level   = "1"
@@ -116,7 +122,8 @@ control "cis_v150_9_7" {
   description   = "Periodically, newer versions are released for Python software either due to security flaws or to include additional functionality. Using the latest Python version for web apps is recommended in order to take advantage of security fixes, if any, and/or additional functionalities of the newer version."
   query         = query.manual_control
   documentation = file("./cis_v150/docs/cis_v150_9_7.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.7"
     cis_level   = "1"
@@ -130,7 +137,8 @@ control "cis_v150_9_8" {
   description   = "Periodically, newer versions are released for Java software either due to security flaws or to include additional functionality. Using the latest Java version for web apps is recommended in order to take advantage of security fixes, if any, and/or new functionalities of the newer version."
   query         = query.manual_control
   documentation = file("./cis_v150/docs/cis_v150_9_8.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.8"
     cis_level   = "1"
@@ -144,7 +152,8 @@ control "cis_v150_9_9" {
   description   = "Periodically, newer versions are released for HTTP either due to security flaws or to include additional functionality. Using the latest HTTP version for web apps to take advantage of security fixes, if any, and/or new functionalities of the newer version."
   query         = query.appservice_web_app_latest_http_version
   documentation = file("./cis_v150/docs/cis_v150_9_9.md")
-
+  severity = "medium"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.9"
     cis_level   = "1"
@@ -158,7 +167,8 @@ control "cis_v150_9_10" {
   description   = "By default, Azure Functions, Web and API Services can be deployed over FTP. If FTP is required for an essential deployment workflow, FTPS should be required for FTP login for all App Service Apps and Functions."
   query         = query.appservice_ftp_deployment_disabled
   documentation = file("./cis_v150/docs/cis_v150_9_10.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.10"
     cis_level   = "1"
@@ -172,7 +182,8 @@ control "cis_v150_9_11" {
   description   = "Encryption keys, Certificate thumbprints and Managed Identity Credentials can be coded into the APP service, this renders them visible as part of the configuration, to maintain security of these keys it is better to store in an Azure Keyvault and reference them from the Keyvault."
   query         = query.manual_control
   documentation = file("./cis_v150/docs/cis_v150_9_11.md")
-
+  severity = "high"
+  
   tags = merge(local.cis_v150_9_common_tags, {
     cis_item_id = "9.11"
     cis_level   = "2"
